524a487361277f9e080a1fa1f89811dead141225
hoyeol Jeong <wesung777@ibizsoftware.net>
2011-11-25 00:31:58 +0900

    second commit

---
+++ src/main/java/com/ibiz/sample/config/WebApp.java
@@ -0,0 +1,26 @@
+package com.ibiz.sample.config;
+
+import javax.servlet.ServletContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebApp implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(final ServletContext servletContext) throws ServletException {
+		final AnnotationConfigWebApplicationContext root = new AnnotationConfigWebApplicationContext();
+		root.setServletContext(servletContext);
+		root.scan("com.ibiz.sample.config");
+		root.refresh();
+
+		final Dynamic servlet = servletContext.addServlet("spring", new DispatcherServlet(root));
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/*");
+	}
+
+}
